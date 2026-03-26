84fa3d625f26ebb9410fdc65913f6c7951116ca5
Manish96386 <manishdas2408@gmail.com>
2026-03-26 16:51:47 +0530

    Added Account base class code

---
+++ src/BankingSystem.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+// Base Class
+abstract class Account {
+    protected int accountNumber;
+    protected String name;
+    protected double balance;
+    protected List<String> transactionHistory = new ArrayList<>();
+
+    public Account(int accountNumber, String name, double balance) {
+        this.accountNumber = accountNumber;
+        this.name = name;
+        this.balance = balance;
+    }
+
+    public abstract void deposit(double amount);
+    public abstract void withdraw(double amount);
+
+    public void checkBalance() {
+        System.out.println("Current Balance: " + balance);
+    }
+
+    public void showTransactions() {
+        System.out.println("Transaction History:");
+        for (String t : transactionHistory) {
+            System.out.println(t);
+        }
+    }
+}
