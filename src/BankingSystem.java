07f985a9c9866bf9c1b7dbfac79e2cd714089d3f
Manish96386 <manishdas2408@gmail.com>
2026-03-26 17:35:51 +0530

    WIP: started SavingsAccount class

--- src/BankingSystem.java
+++ src/BankingSystem.java
@@ -27,3 +27,9 @@ abstract class Account {
         }
     }
 }
+
+class SavingsAccount extends Account {
+
+    public SavingsAccount(int accNo, String name, double balance) {
+        super(accNo, name, balance);
+    }
